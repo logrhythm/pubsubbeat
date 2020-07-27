ee1fbbd280ef89e641e515e11d1f6485a2c2362a
himanshu8859 <rajan.joshi@logrhythm.com>
2020-07-27 16:13:58 +0530

    path changes done

--- beater/pubsubbeat.go
+++ beater/pubsubbeat.go
@@ -28,7 +28,7 @@ import (
 
 	"encoding/json"
 
-	"github.com/logrhythm/pubsubbeat/external/crypto"
+	"github.com/logrhythm/pubsubbeat/crypto"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
