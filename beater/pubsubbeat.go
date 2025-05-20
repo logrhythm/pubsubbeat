d9bf3e58f41027f526560d531baaf23a06c7c527
pramit <pramit.kumar@logrhythm.com>
2025-05-19 21:14:18 -0700

    print log

--- beater/pubsubbeat.go
+++ beater/pubsubbeat.go
@@ -157,6 +157,7 @@ func (bt *Pubsubbeat) Run(b *beat.Beat) error {
 			var unmarshalErr error
 			if bt.config.Json.FieldsUnderRoot {
 				unmarshalErr = json.Unmarshal(m.Data, &eventMap)
+				bt.logger.Info("pubsubbeat eventMap 160:-", eventMap)
 				if unmarshalErr == nil && bt.config.Json.FieldsUseTimestamp {
 					var timeErr error
 					timestamp := eventMap[bt.config.Json.FieldsTimestampName]
@@ -188,7 +189,7 @@ func (bt *Pubsubbeat) Run(b *beat.Beat) error {
 		if datetime.IsZero() {
 			datetime = time.Now()
 		}
-		bt.logger.Info("pubsubbeat eventMap.", eventMap)
+
 		bt.client.Publish(beat.Event{
 			Timestamp: datetime,
 			Fields:    eventMap,
